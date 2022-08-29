f03ffdf330f6ca17a9a7ce9329711069379b57a3
Sami NEFZI <sami.nefzi@gmail.com>
2022-08-29 16:46:29 +0200

    Start webapp

---
+++ src/test/java/fr/m2i/javaspringmvc/JavaspringmvcApplicationTests.java
@@ -0,0 +1,12 @@
+package fr.m2i.javaspringmvc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringmvcApplicationTests {
+
+	//@Test
+	//void contextLoads() {
+	//}
+
+}
